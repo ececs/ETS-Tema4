12af8f16c5b3634d31504565cd1c77314008fdc2
Eudaldo A Cal Saul <eudaldocal@gmail.com>
2025-02-18 20:00:48 +0000

    Añadimos una foto con cambio en el retiro y el ingreso

--- CCuenta.java
+++ CCuenta.java
@@ -113,7 +113,7 @@ public class CCuenta {
     public static void main(String[] args) {
     CCuenta cuenta1;
     double saldoActual;
-    cuenta1 = new CCuenta("Eudaldo Alvaro Cal Saul","1000-2365-85-1230456789",2500,0);
+    cuenta1 = new CCuenta("Eudaldo Cal Saul","1000-2365-85-1230456789",3000,0);
     saldoActual = cuenta1.estado();
     System.out.println("El saldo actual es "+ saldoActual );
     operaciones(cuenta1);
